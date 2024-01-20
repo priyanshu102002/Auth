255f6bd84b861a32c4f148b1fdacd66020e8857f
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-20 13:32:25 +0530

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI).then(() => { console.log('Connected to MongoDB') }).catch((err) => { console.log(err) });
 
 
 
