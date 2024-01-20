79c56f7e7a982deb9cc5cbff6d1225d56ff2901b
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-20 17:43:26 +0530

    user modal created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <div className="bg-slate-300">
-            <div className="flex justify-between p-4 items-center">
+            <div className="flex justify-between px-8 py-6 text-xl items-center">
                 <Link to="/">
                     <h1 className="font-bold">Auth</h1>
                 </Link>
-                <ul className="flex gap-4">
+                <ul className="flex gap-8">
                     <Link to="/">
                         <li>Home</li>
                     </Link>
