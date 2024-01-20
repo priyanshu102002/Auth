cc65331ac8271d9cc3b5a536c3b75a8d4c1d2a12
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-20 12:12:40 +0530

    creating header pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,19 @@ import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import About from "./pages/About";
 
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/sign-in" element={<SignIn />} />
                 <Route path="/sign-up" element={<SignUp />} />
                 <Route path="/profile" element={<Profile />} />
-                <Route path="/profile" element={<Profile />} />
+                <Route path="/about" element={<About />} />
             </Routes>
         </BrowserRouter>
     );
