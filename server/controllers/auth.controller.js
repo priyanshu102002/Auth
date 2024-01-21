d3e17e7a1caeaee3729278ed64d72b6b0e4a3c56
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-21 10:34:06 +0530

    sign in api routes created

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { customErrorHandler } from "../utils/error.utils.js";
 
 export const signup = async (req, res, next) => {
 
@@ -19,5 +21,45 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+};
 
+export const signin = async (req, res, next) => {
+
+    // Taking the data from user
+    const { email, password } = req.body;
+
+    try {
+        // Checking if the user exists or not
+        const validUser = await User.findOne({ email });
+
+        // If the user doesn't exist
+        if (!validUser) {
+            return next(customErrorHandler("User not found", 404));
+        }
+
+        // comparing the password
+        const isValidPassword = await bcryptjs.compare(password, validUser.password);
+
+        // If the password is not correct
+        if (!isValidPassword) {
+            return next(customErrorHandler("Wrong credentials", 401));
+        }
+
+        // Removing password from the user object to send it to the client
+        const { password: hashedPassword, ...rest } = validUser._doc;
+
+        // generating the token
+        const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+        // If email and password is correct then we will send the token via cookie 
+        const cookieOptions = {
+            expires: new Date(Date.now() + 60 * 60 * 1000), //1hr
+            httpOnly: true
+        }
+        res.cookie("access_token", token, cookieOptions).status(200).json(rest)
+
+
+    } catch (error) {
+        next(error);
+    }
 };
