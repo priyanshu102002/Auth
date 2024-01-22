534afabf3560eb204a1594be05bd1379e72ad499
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-22 09:53:43 +0530

    protected routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import About from "./pages/About";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
     return (
@@ -14,7 +15,9 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/sign-in" element={<SignIn />} />
                 <Route path="/sign-up" element={<SignUp />} />
-                <Route path="/profile" element={<Profile />} />
+                <Route element={<PrivateRoute />}>
+                    <Route path="/profile" element={<Profile />} />
+                </Route>
                 <Route path="/about" element={<About />} />
             </Routes>
         </BrowserRouter>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+    const { currentUser } = useSelector((state) => state.user);
+    console.log(currentUser);
     return (
         <div className="bg-slate-300">
             <div className="flex justify-between px-8 py-6 text-xl items-center">
                 <Link to="/">
                     <h1 className="font-bold">Auth</h1>
                 </Link>
-                <ul className="flex gap-8">
+                <ul className="flex gap-8 items-center">
                     <Link to="/">
                         <li>Home</li>
                     </Link>
                     <Link to="/about">
                         <li>About</li>
                     </Link>
-                    <Link to="/sign-in">
-                        <li>SignIn</li>
+                    <Link to="/profile">
+                        {currentUser ? (<img className="rounded-full w-7 h-7 object-cover" src={currentUser.profilePicture} />) : <li>Sign In</li>}
                     </Link>
                 </ul>
             </div>
